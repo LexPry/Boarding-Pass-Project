1accd29a8b034ac1806218a581ec6f7aa1772203
Anikulapoo <102306405+Anikulapoo@users.noreply.github.com>
2022-05-02 15:24:46 -0400

    Update WriteToFile.java

--- src/WriteToFile.java
+++ src/WriteToFile.java
@@ -5,13 +5,15 @@ import java.io.PrintWriter;
 import java.util.Date;
 
 public class WriteToFile {
-    public static void saveRecord(BoardingPass boardingPass , String name, String email, String gender, String phoneNumber, Date date, String destination, Date departureTime, String origin, int age) throws IOException {
+    public static void saveRecord(BoardingPass boardingPass ,User user) throws IOException {
         String filepath = "resource/user_data.csv";
         FileWriter fw = new FileWriter(filepath, true);
         BufferedWriter bw = new BufferedWriter(fw);
         PrintWriter pw = new PrintWriter(bw);
 
-        pw.println(boardingPass.getUser().getBoardingPassNumber()+","+ name+","+email+","+phoneNumber+","+gender+","+age+","+date+","+origin+","+destination+","+departureTime);
+        pw.println(boardingPass.getBoardingPassNumber()+","+ user.getName()+","+user.getEmail()+","+
+                user.getPhoneNumber()+","+user.getGender()+","+user.getAge()+","+user.getDate()+","+
+                user.getOrigin()+","+user.getDestination()+","+user.getDepartureTime()+","+boardingPass.getETA());
         pw.flush();
         pw.close();
     }
