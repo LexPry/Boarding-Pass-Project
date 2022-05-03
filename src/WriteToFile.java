1b257b676cfb1532bf154d0c88e8c9430ccae1dd
Robert C. Kowalczyk <catdogramb@gmail.com>
2022-05-03 14:31:58 -0400

    Unit Testing for BoardingPass.  Also slight change to public access required small change in WriteToFile

--- src/WriteToFile.java
+++ src/WriteToFile.java
@@ -11,7 +11,7 @@ public class WriteToFile {
         BufferedWriter bw = new BufferedWriter(fw);
         PrintWriter pw = new PrintWriter(bw);
 
-        pw.println(boardingPass.getBoardingPassNumber()+","+ user.getName()+","+user.getEmail()+","+
+        pw.println(boardingPass.getBoardingNumber()+","+ user.getName()+","+user.getEmail()+","+
                 user.getPhoneNumber()+","+user.getGender()+","+user.getAge()+","+user.getDate()+","+
                 user.getOrigin()+","+user.getDestination()+","+user.getDepartureTime()+","+boardingPass.getETA());
         pw.flush();
---
+++ test/BoardingPassTest.java
@@ -0,0 +1,52 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardingPassTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBoardingNumber() {
+        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy hh:mm aa");
+
+        Date date = assertDoesNotThrow(()-> format.parse("05/03/2022 11:01 am"));
+        User testUserMale = new User("Crash Test Dummy", "crashtest@airplanesRsafe.com", "male", "555-555-5555",
+                date, "LAX", date, "JFK", 50);
+        User testUserSeniorFemale = new User("Crash Test Dummette", "areufosreal@airplanesRsafe.com", "f", "555-555-5556",
+                date, "ORD", date, "JFK", 70);
+        User testUserKidMale = new User("Crash Test Timmy", "iamtooyoungtohaveanemail@airplanesRsafe.com", "male", "555-555-5556",
+                date, "ORD", date, "JFK", 3);
+
+        BoardingPass male = assertDoesNotThrow(()->  new BoardingPass(testUserMale));
+        BoardingPass female = assertDoesNotThrow(()->  new BoardingPass(testUserSeniorFemale));
+        BoardingPass kid = assertDoesNotThrow(()->  new BoardingPass(testUserKidMale));
+
+        assertEquals( (int) BoardingPass.getAirports().get("JFK").basePrices.get("LAX"), male.getFinalTicketPrice());
+        assertEquals( (int)(BoardingPass.getFemaleDiscountPercent()* BoardingPass.getSeniorDiscountPercent()
+                * BoardingPass.getAirports().get("JFK").basePrices.get("ORD")), female.getFinalTicketPrice());
+        assertEquals( (int)(BoardingPass.getKidDiscountPercent()
+                * BoardingPass.getAirports().get("JFK").basePrices.get("ORD")), kid.getFinalTicketPrice());
+
+        assertEquals( 20, male.getBoardingNumber().length());
+        var e =kid.getETA().getTime() - date.getTime();
+        var f = BoardingPass.getAirports().get("JFK").estFlightTimes.get("ORD") * 60 * 1000; //convert minutes to milliseconds
+        assertEquals(f, e);
+
+        assertFalse(kid.isFemaleDiscount());
+        assertTrue(kid.isKidDiscount());
+        assertFalse(kid.isSeniorDiscount());
+        assertTrue(female.isSeniorDiscount());
+        assertFalse(female.isKidDiscount());
+        assertTrue(female.isFemaleDiscount());
+        assertFalse(male.isSeniorDiscount());
+        assertFalse(male.isKidDiscount());
+        assertFalse(male.isFemaleDiscount());
+    }
+}
