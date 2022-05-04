87bcea2d80e91eddecbcf8b4b615bed57d3669b7
Anikulapoo <102306405+Anikulapoo@users.noreply.github.com>
2022-05-04 11:20:09 -0400

    Slight update to BoardingPass.java
    made getBoardingPassNumber public so I could access it in the write to file class.

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -113,7 +113,7 @@ public class BoardingPass {
 
     private final String boardingNumber;
 
-    private String getBoardingPassNumber() {
+    public String getBoardingPassNumber() {
        //combine all information into a single String, then hash, then take use 20 digits of hash
         StringBuilder sb = new StringBuilder();
 
