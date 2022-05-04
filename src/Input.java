ed5c43374c09825c3e0ce23e4ea99822a3751804
pryme <akexboswell8@gmail.com>
2022-05-04 17:52:00 -0400

    Fixed date so it outputs the right departure time, not entirely sure how to omit the seconds

--- src/Input.java
+++ src/Input.java
@@ -1,4 +1,7 @@
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Input {
@@ -21,21 +24,15 @@ public class Input {
         System.out.println("Please enter your gender: ");
         // user input for gender
         User.Gender gender = null;
-        String inGender = in.next();
-
-        if (inGender.equalsIgnoreCase("m")) {
-            gender = User.Gender.M;
-        } else if (inGender.equalsIgnoreCase("f")) {
-            gender = User.Gender.F;
-        }
+        gender = InputValidation.checkGender(in);
 
         System.out.println("Please enter your email: ");
         String email = in.next();
 
         System.out.println("Please Enter your phone number: ");
-        String phoneNumber = in.nextLine();
+        String phoneNumber = in.next();
 
-        while (!checkNumber(phoneNumber)) {
+        while (!InputValidation.checkNumber(phoneNumber)) {
             System.out.println("Please enter a valid phone number: ");
             phoneNumber = in.next();
         }
@@ -52,43 +49,15 @@ public class Input {
         System.out.println("Expected Format: Mon/Day/Year");
         String line = in.next();
         String[] dateFormatter = line.split("/");
-        Date departureTime = new Date(Integer.parseInt(dateFormatter[0]), // 12/21/2019 -> Dec/ 21st/ 2019 19:00:
+        Calendar calendar = new GregorianCalendar(Integer.parseInt(dateFormatter[2]), // 12/21/2019 -> Dec/ 21st/ 2019 19:00:
                 Integer.parseInt(dateFormatter[1]),
-                Integer.parseInt(dateFormatter[2]));
+                Integer.parseInt(dateFormatter[0]));
+        Date departureTime = new Date(String.valueOf(calendar.getTime()));
 
         System.out.println("Please enter your current location: ");
-        String origin = "LAX";
+        String origin = in.next();
 
         return new User(name, email, gender, phoneNumber, date, destination, departureTime, origin, age);
-    }
-
-    public static boolean checkNumber(String phoneNumber) {
-        if (phoneNumber.length() >= 9) {
-            // validate phone numbers of format "1234567890"
-            if (phoneNumber.matches("\\d{10}"))
-                return true;
-                // validating phone number with -, . or spaces
-            else if (phoneNumber.matches("\\d{3}[-.\\s]\\d{3}[-.\\s]\\d{4}"))
-                return true;
-                // validating phone number with extension length from 3 to 5
-            else if (phoneNumber.matches("\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}"))
-                return true;
-                // validating phone number where area code is in braces ()
-            else if (phoneNumber.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}"))
-                return true;
-                // Validation for India numbers
-            else if (phoneNumber.matches("\\d{4}[-.\\s]\\d{3}[-.\\s]\\d{3}"))
-                return true;
-            else if (phoneNumber.matches("\\(\\d{5}\\)-\\d{3}-\\d{3}"))
-                return true;
-
-            else if (phoneNumber.matches("\\(\\d{4}\\)-\\d{3}-\\d{3}"))
-                return true;
-                // return false if nothing matches the input
-            else
-                return false;
-        }
-        return false;
     }
 
 }
---
+++ src/InputValidation.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class InputValidation {
+    public static boolean checkNumber(String phoneNumber) {
+        if (phoneNumber.length() >= 9) {
+            // validate phone numbers of format "1234567890"
+            if (phoneNumber.matches("\\d{10}")) {
+                return true;
+            }
+            // validating phone number with -, . or spaces
+            else if (phoneNumber.matches("\\d{3}[-.\\s]\\d{3}[-.\\s]\\d{4}")) {
+                return true;
+            }
+            // validating phone number with extension length from 3 to 5
+            else if (phoneNumber.matches("\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}")) {
+                return true;
+            }
+            // validating phone number where area code is in braces ()
+            else if (phoneNumber.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}")) {
+                return true;
+            }
+            // Validation for India numbers
+            else if (phoneNumber.matches("\\d{4}[-.\\s]\\d{3}[-.\\s]\\d{3}")) {
+                return true;
+            } else // return false if nothing matches the input
+                if (phoneNumber.matches("\\(\\d{5}\\)-\\d{3}-\\d{3}")) {
+                    return true;
+                } else return phoneNumber.matches("\\(\\d{4}\\)-\\d{3}-\\d{3}");
+        }
+        return false;
+    }
+
+    public static User.Gender checkGender(Scanner in) {
+        System.out.println("Please choose a gender:  \n 1. Male \n2. Female \n3. Prefer not to say");
+        String temp = in.next();
+        User.Gender gender;
+        // only take in one number, no whitespace, no chars
+        while (!temp.matches("(?<!\\S)\\d(?!\\S)")) {
+            temp = in.next();
+        }
+
+        switch (Integer.parseInt(temp)) {
+            case 1:
+                gender = User.Gender.M;
+                break;
+            case 2:
+                gender = User.Gender.F;
+                break;
+            default:
+                gender = User.Gender.X;
+        }
+        return gender;
+    }
+}
