bdba0b297639001bf260040b54179e48d268f237
Ian Carper <Itcarper@ymail.com>
2022-05-06 16:55:23 -0500

    re-did after date format change

--- src/Formatting.java
+++ src/Formatting.java
@@ -19,8 +19,13 @@ public class Formatting {
             while (sc.hasNext()) {
                 //split userdata into 11 elements
                 String[] userData = sc.nextLine().split(",");
+<<<<<<< Updated upstream
                 if (!sc.hasNext()) {
                     String topSpace = String.format("%98s", "").replace(' ', '=');
+=======
+                if (!sc.hasNextLine()) {
+                    String topSpace = String.format("%105s", "").replace(' ', '=');
+>>>>>>> Stashed changes
                     String passNumber = userData[0];
                     String name = userData[1];
                     String email = userData[2];
@@ -30,29 +35,49 @@ public class Formatting {
                     String date = userData[6].substring(0, 10) + " " + userData[6].substring(24, 28);
                     String origin = userData[7];
                     String destination = userData[8];
+<<<<<<< Updated upstream
                     String departureTime = converToStandardTime(userData[9].substring(11, 16));
+=======
+                    String departureTime = userData[9].substring(0, 11) + converToStandardTime(userData[9].substring(11, 16));
+>>>>>>> Stashed changes
                     String eta = userData[10].substring(0, 11) + converToStandardTime(userData[10].substring(11,16));
                     String totalPrice = NumberFormat.getCurrencyInstance(new Locale("en", "US")).format(Float.parseFloat(userData[11])/100);
-                    String bottomSpace = String.format("%98s", "").replace(' ', '=');
+                    String bottomSpace = String.format("%105s", "").replace(' ', '=');
 
-                    //formatted to look like a ticket,every 6 lines is a new ticket
+                    //formatted to look like a ticket
                     formattedWrite.write(String.format("%s%n" +
+<<<<<<< Updated upstream
                                     "|Name:  %-31s Age: %-24s Gender: %-19s|%n" +
                                     "|Email: %-31s Phone Number: %-15s Departure Time: %-11s|%n" +
                                     "|From:  %-31s To:  %-24s Date: %-21s|%n" +
                                     "|Boarding Pass #: %-21s ETA: %-24s Price: %-20s|%n" +
                                     "%s%n",
+=======
+                                    "|Name:  %-31s Age: %-24s Gender: %-26s|%n" +
+                                    "|Email: %-31s Phone Number: %-15s Time of flight: %-18s|%n" +
+                                    "|From:  %-31s To:  %-24s Date of print-out: %-12s|%n" +
+                                    "|Boarding Pass #: %-21s ETA: %-24s Price: %-27s|%n" +
+                                    "%s%n%n",
+>>>>>>> Stashed changes
                             topSpace, name, age, gender,
                             email, phoneNumber, departureTime,
                             origin, destination, date,
                             passNumber, eta, totalPrice, bottomSpace));
                     //print to console
                     System.out.printf("%s%n" +
+<<<<<<< Updated upstream
                                     "|Name:  %-31s Age: %-24s Gender: %-19s|%n" +
                                     "|Email: %-31s Phone Number: %-15s Departure Time: %-11s|%n" +
                                     "|From:  %-31s To:  %-24s Date: %-21s|%n" +
                                     "|Boarding Pass #: %-21s ETA: %-24s Price: %-20s|%n" +
                                     "%s%n",
+=======
+                                    "|Name:  %-31s Age: %-24s Gender: %-26s|%n" +
+                                    "|Email: %-31s Phone Number: %-15s Time of flight: %-18s|%n" +
+                                    "|From:  %-31s To:  %-24s Date of print-out: %-12s|%n" +
+                                    "|Boarding Pass #: %-21s ETA: %-24s Price: %-27s|%n" +
+                                    "%s%n%n",
+>>>>>>> Stashed changes
                             topSpace, name, age, gender,
                             email, phoneNumber, departureTime,
                             origin, destination, date,
@@ -63,6 +88,19 @@ public class Formatting {
         }catch (Exception e){
             e.printStackTrace();
         }
+
+    }
+    public static String converToStandardTime(String time){
+        //since user data comes in military time if the first position is larger than 2 or equal to zero then it is guaranteed to be in the morning
+        if(Integer.parseInt(time.substring(0,1)) == 0){
+            time = time.substring(1,5) + " AM";
+
+        }else if(Integer.parseInt(time.substring(0,1)) == 2 || Integer.parseInt(time.substring(1,2)) > 2){
+                time = Integer.parseInt(time.substring(0,2))-12 + time.substring(2,5) + " PM";
+        }else{
+            time+= " AM";
+        }
+        return time;
     }
     public static String converToStandardTime(String time){
         //since user data comes in military time if the first position is larger than 2 or equal to zero then it is guaranteed to be in the morning
