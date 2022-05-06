281bb302e1f24b4aa04a751201260bcdd1bba3b5
Ian Carper <Itcarper@ymail.com>
2022-05-06 13:04:07 -0500

    removed user string in main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         User user = Input.getInformation();
         BoardingPass boardingPass = new BoardingPass(user);
-        System.out.println(user);
         WriteToFile.saveRecord(boardingPass,user);
         Formatting.format();
     }
